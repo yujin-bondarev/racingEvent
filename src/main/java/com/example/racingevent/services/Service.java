14c51f5ae78984b6d6296ff7696cd580d9a3566d
yujin-bond <evgenij.bondarev.2004@mail.ru>
2024-04-21 12:14:26 +0300

    creating abstract service

---
+++ src/main/java/com/example/racingevent/services/Service.java
@@ -0,0 +1,18 @@
+package com.example.racingevent.services;
+
+import com.example.racingevent.model.entity.AbstractEntity;
+
+import java.util.List;
+
+public interface Service<T extends AbstractEntity> {
+
+    T read(Long id);
+
+    List<T> read();
+
+    void save(T entity);
+
+    void delete(Long id);
+
+    void edit (T entity);
+}

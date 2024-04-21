694b932371d199db4d503b7c940fd95b72220696
yujin-bond <evgenij.bondarev.2004@mail.ru>
2024-04-21 12:07:42 +0300

    Creating abstract class for entity and abstract contoller

---
+++ src/main/java/com/example/racingevent/model/entity/AbstractEntity.java
@@ -0,0 +1,45 @@
+package com.example.racingevent.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractEntity other = (AbstractEntity) obj;
+        if (id == null) {
+            return other.id == null;
+        } else return id.equals(other.id);
+    }
+
+
+}
---
+++ src/main/java/com/example/racingevent/web/AbstractController.java
@@ -0,0 +1,61 @@
+package com.example.racingevent.web;
+
+import com.example.racingevent.model.entity.AbstractEntity;
+import com.example.racingevent.services.Service;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public abstract class AbstractController<T extends AbstractEntity> {
+    protected HttpHeaders headers;
+
+    @PostConstruct
+    private void init() {
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<T>> get() {
+        List<T> entities = getService().read();
+        if (entities.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(entities, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<T> getById(@PathVariable long id) {
+        T entity = getService().read(id);
+        if (entity == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(entity, headers, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> put(@RequestBody T entity) {
+        getService().save(entity);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> post(@RequestBody T entity) {
+        getService().save(entity);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable long id) {
+        getService().delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public abstract Service<T> getService();
+}
+
